556bc88b4d15d3fe271fb281b89c454ba6926f3e
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-01-23 18:34:54 +0100

    updated to fi flake8,pylint, mypy. Implement to show only the valid coordinates, when pick already used

--- tic_tac_toe/tic_tac_toe/tests/tic_tac_toe_test.py
+++ tic_tac_toe/tic_tac_toe/tests/tic_tac_toe_test.py
@@ -1,34 +1,32 @@
-from typing import Any, Tuple, List
-from tic_tac_toe.tic_tac_toe.tic_tac_toe import \
-    get_winning_player, check_board_full 
+from typing import Optional, Tuple, List
 import pytest
+from tic_tac_toe.tic_tac_toe.tic_tac_toe import \
+    get_winning_player, check_board_full
 
 
-test_winning: List[Tuple[List[List[str]],str, bool]] = [
-    ([["X","X","X"],[".",".","."],[".",".","."]], "X", "X"),
-    ([[".",".","."],["X","X","X"],[".",".","."]], "X", "X"),
-    ([[".",".","."],[".",".","."],["X","X","X"]], "X", "X"),
-    ([["X",".","."],[".","X","."],[".",".","X"]], "X", "X"),
-    ([[".",".","X"],[".","X","."],["X",".","."]], "X", "X"),
-    ([["X",".","."],["X",".","."],["X",".","."]], "X", "X"),
-    ([[".","X","."],[".","X","."],[".","X","."]], "X", "X"),
-    ([[".",".","X"],[".",".","X"],[".",".","X"]], "X", "X"),
-    ([["X","X","O"],[".","O","."],["O",".","."]], "X", None),
-    ([["X","O","X"],[".",".","."],[".","O","O"]], "X", None),
-    ([["X",".","X"],[".",".","."],[".",".","."]], "X", None)
+test_winning: List[Tuple[List[List[str]], str, Optional[str]]] = [
+    ([["X", "X", "X"], [".", ".", "."], [".", ".", "."]], "X", "X"),
+    ([[".", ".", "."], ["X", "X", "X"], [".", ".", "."]], "X", "X"),
+    ([[".", ".", "."], [".", ".", "."], ["X", "X", "X"]], "X", "X"),
+    ([["X", ".", "."], [".", "X", "."], [".", ".", "X"]], "X", "X"),
+    ([[".", ".", "X"], [".", "X", "."], ["X", ".", "."]], "X", "X"),
+    ([["X", ".", "."], ["X", ".", "."], ["X", ".", "."]], "X", "X"),
+    ([[".", "X", "."], [".", "X", "."], [".", "X", "."]], "X", "X"),
+    ([[".", ".", "X"], [".", ".", "X"], [".", ".", "X"]], "X", "X"),
+    ([["X", "X", "O"], [".", "O", "."], ["O", ".", "."]], "X", None),
+    ([["X", "O", "X"], [".", ".", "."], [".", "O", "O"]], "X", None),
+    ([["X", ".", "X"], [".", ".", "."], [".", ".", "."]], "X", None)
 ]
 
 test_board_full: List[Tuple[List[List[str]], bool]] = [
-    ([["X","X","X"],[".",".","."],[".",".","."]], False),
-    ([[".",".","."],["X","X","X"],[".",".","."]], False),
-    ([["X","X","X"],[".",".","."],["X","X","X"]], False),
-    ([["X","X","X"],["O","O","O"],["X","O","X"]], True),
-    ([["O","O","O"],[".",".","."],[".",".","."]], False),
-    ([["O","O","O"],["O","O","O"],[".",".","."]], False),
-    ([["X","X","X"],["O","O","O"],["O","O","O"]], True),
-    ([[".",".","."],[".",".","."],[".",".","."]], False)
-    
-
+    ([["X", "X", "X"], [".", ".", "."], [".", ".", "."]], False),
+    ([[".", ".", "."], ["X", "X", "X"], [".", ".", "."]], False),
+    ([["X", "X", "X"], [".", ".", "."], ["X", "X", "X"]], False),
+    ([["X", "X", "X"], ["O", "O", "O"], ["X", "O", "X"]], True),
+    ([["O", "O", "O"], [".", ".", "."], [".", ".", "."]], False),
+    ([["O", "O", "O"], ["O", "O", "O"], [".", ".", "."]], False),
+    ([["X", "X", "X"], ["O", "O", "O"], ["O", "O", "O"]], True),
+    ([[".", ".", "."], [".", ".", "."], [".", ".", "."]], False)
 ]
 
 
@@ -36,6 +34,7 @@ test_board_full: List[Tuple[List[List[str]], bool]] = [
 def test_check_winning(board, player, win):
     assert get_winning_player(board, player) == win
 
+
 @pytest.mark.parametrize("board, full", test_board_full)
 def test_check_board_full(board, full):
-    assert check_board_full(board) == full   
+    assert check_board_full(board) == full
--- tic_tac_toe/tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe/tic_tac_toe.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from typing import Dict, List, Tuple
+from typing import Dict, List, Optional, Tuple
 
 
 def get_empty_board() -> List[List[str]]:
@@ -24,11 +24,10 @@ def set_player_avatar() -> str:
 def change_player(player: str) -> str:
     if player == "X":
         return "O"
-    else:
-        return "X"
+    return "X"
 
 
-def get_human_coordinates(board: List[List[str]]) -> Tuple[int, int]:
+def get_human_coordinates(board: List[List[str]],used_list: List[str]) -> Tuple[str, Tuple[int, int]]:
     valid_coordinates: Dict[str, Tuple[int, int]] = {
         "A1": (0, 0),
         "A2": (0, 1),
@@ -40,52 +39,52 @@ def get_human_coordinates(board: List[List[str]]) -> Tuple[int, int]:
         "C2": (2, 1),
         "C3": (2, 2)
     }
-    valid_coordinates_list: List[str] = [
-        key for key in valid_coordinates.keys()]
+    valid_coordinates_list: List[str] = list(valid_coordinates.keys())
     coordinates_are_valide: bool = False
     while not coordinates_are_valide:
         human_coordinates: str = input("Give Your coordinates: ").upper()
         if human_coordinates == "QUIT":
             sys.exit()
-        if human_coordinates in valid_coordinates.keys():
-            coordinates: Tuple[int, int] = valid_coordinates.get(
-                human_coordinates)
-            if board[coordinates[0]][coordinates[1]] == ".":
-                coordinates_are_valide = True
-                return coordinates
-            else:
-                os.system('clear')
-                print_board(board)
-                print("Please give a not taken coordinat! Use: " +
-                    ", ".join(valid_coordinates_list))    
-        else:
+        if human_coordinates not in valid_coordinates: #keys removed
             os.system('clear')
             print_board(board)
-            print("Please give a valid coordinat! Use: " +
+            print("Please give a valid coordinate! Use: " +
                   ", ".join(valid_coordinates_list))
+        elif human_coordinates in valid_coordinates: #keys removed
+            coordinates: Tuple[int, int] = valid_coordinates.get(
+                human_coordinates)
+            if board[coordinates[0]][coordinates[1]] != ".":
+                os.system('clear')
+                print_board(board)
+                print("Please give a not taken coordinate! Use: " +
+                      ", ".join([item for item in valid_coordinates_list if item not in used_list]))
+            else:
+                coordinates_are_valide = True
+                return human_coordinates, coordinates
 
 
 def marking_move(player: str, board: List[List[str]], move: Tuple[int, int]) -> List[List[str]]:
     board[move[0]][move[1]] = player
     return board
 
-def get_winning_player(board: List[List[str]], player: str) -> str:
-    #win for horizontal
+
+def get_winning_player(board: List[List[str]], player: str) -> Optional[str]:
+    # win for horizontal
     for row in board:
         if row.count(player) == 3:
             return player
 
-    #win for vertical
+    # win for vertical
     counter: int = 0
-    if ["X", "O"] in board[0]:
-        index_:int = board[0].index(player)
+    if board[0] == "X" or "O":
+        index_: int = board[0].index(player)
         for row in board:
             if row[index_] == player:
                 counter += 1
         if counter == 3:
-            return player    
+            return player
 
-    #win for diagonal from 0,0
+    # win for diagonal from 0,0
     counter = 0
     for i, row in enumerate(board):
         if row[i] == player:
@@ -93,7 +92,7 @@ def get_winning_player(board: List[List[str]], player: str) -> str:
     if counter == 3:
         return player
 
-    #win for diagonal from 0,2
+    # win for diagonal from 0,2
     counter = 0
     for i, row in enumerate(board):
         if row[len(row)-1-i] == player:
@@ -102,19 +101,20 @@ def get_winning_player(board: List[List[str]], player: str) -> str:
         return player
     return None
 
+
 def check_board_full(board: List[List[str]]) -> bool:
     for row in board:
         if "." in row:
             return False
-    else:
-        return True       
+
+    return True
 
 
 def print_board(board: List[List[str]]) -> None:
     print("   1   2   3")
     separator_pattern: str = "---+---+---"
     row_index: List[str] = ["A", "B", "C"]
-    for i,row in enumerate(board):
+    for i, row in enumerate(board):
         print(f"{row_index[i]}  " + ' | '.join(row))
         if i != len(board) - 1:
             print(f"  {separator_pattern}")
@@ -128,25 +128,27 @@ def print_game_result(player: str) -> None:
     else:
         print("It's a tie!")
 
+
 def tic_tac_toe() -> None:
     board: List[List[str]] = get_empty_board()
     player: str = "X"
     wait_for_winner = True
+    used_cooridinates: List[str] = []
     while not check_board_full(board) and wait_for_winner:
         print_board(board)
         print(f"It's Player {player}'s turn!")
-        move: Tuple[int, int] = get_human_coordinates(board)
+        result_of_human_coordinates: Tuple[str, Tuple[int, int]] = get_human_coordinates(board, used_cooridinates)
+        move: Tuple[int, int] = result_of_human_coordinates[1]
+        used_cooridinates.append(result_of_human_coordinates[0])
+        
         board = marking_move(player, board, move)
         if get_winning_player(board, player):
             os.system('clear')
             print_board(board)
             print_game_result(player)
             wait_for_winner = False
-        else:    
+        else:
             player = change_player(player)
-            os.system('clear') 
+            os.system('clear')
 
 tic_tac_toe()
-
-
-
